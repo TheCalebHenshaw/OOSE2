de1cc0d704c22cafedc18cbdcc34e3bfa5559391
Caleb Henshaw <2810458H@student.gla.ac.uk>
2024-01-12 11:52:15 +0000

    Update FileReader.java

--- FileReader.java
+++ FileReader.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class FileReader {
     public static void main(String[] args){
         int count = 0;
-        String toFind = "brycked";
-        File myfile = new File("/Users/caleb/Desktop/script.txt");
+        String toFind = "red";
+        File myfile = new File("C:/Users/Caleb/Desktop/scripts.txt");
         try{
             Scanner myreader = new Scanner(myfile);
             while(myreader.hasNextLine()){
                 String line = myreader.nextLine().toLowerCase();
                 if(line.contains(toFind)){
                     int lindex = line.indexOf(toFind);
-                    if(!Character.isLetterOrDigit(line.charAt(lindex - 1)) && !Character.isLetterOrDigit(line.charAt(lindex+toFind.length()))){
+                    if((!Character.isLetterOrDigit(line.charAt(lindex - 1)) || lindex==0) && (!Character.isLetterOrDigit(line.charAt(lindex+toFind.length())))){
                         System.out.println("line with alphabet "+line);
                         count++;
                     }
                     System.out.println("----CURRENT LINE TO COUNT--- \n"+line);
-                    System.out.println("the index of apple is " + lindex);
+                    System.out.println("the index of --" + toFind + "-- is " + lindex);
                 }
 
             }
